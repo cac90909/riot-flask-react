03340403797cb8dd9a72fd68aa197cedd9bb8f89
Chris Castro <cacastro909@gmail.com>
2021-06-22 16:46:29 -0400

    Updated Game.js to display position info

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
+/*https://codewithnico.com/react-wait-axios-to-render/ is a very good link to follow if the async stuff is being weird*/
+
 function Game({game_data}) {
     console.log("Game.js is rendering")
-    console.log("game_data: ", game_data)
-    console.log("game_data.Events: ", game_data.Events)
-    console.log("game_data.Events[0]: ", game_data.Events[0])
-    console.log("game_data.Events[0].timestamp: ", game_data.Events[0].timestamp)
-    console.log("game_data.Events[0].timestamp: ", game_data.Events[1].timestamp)
-    console.log("game_data.Events[0].timestamp: ", game_data.Events[2].timestamp)
+    console.log("game_data object  in Game.js: ", game_data)
+    
+    var timestamp_data = game_data.Timestamps[0]
+
+    for (var player in timestamp_data){
+        for (var extra_layer in timestamp_data[player]){
+            let player_pos_info = timestamp_data[player][extra_layer].position
+            console.log(player_pos_info)
+        }
+    }
+
+
     return <div>
         <p>hi</p>
         <p>hi again</p>
@@ -15,3 +23,11 @@ function Game({game_data}) {
 }
 
 export {Game}
+
+
+/*As of now, I know how to access the data I want. Now, I am going to use that data to display some basic plots. I am planning on using the d3.js library for these visualizations.
+Check out these 3 videos for examples on how to use the library:
+https://www.youtube.com/watch?v=aHJCt2adSWA&ab_channel=Academind
+https://www.youtube.com/watch?v=zXBdNDnqV2Q&ab_channel=ReactConferencesbyGitNation
+https://www.youtube.com/watch?v=AhD-oziq53w&ab_channel=LivelyLab */
+
