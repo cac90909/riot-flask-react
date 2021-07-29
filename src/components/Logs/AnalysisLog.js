3200d7253cadfaef77d862b8a59bef72d5517350
Chris Castro <cacastro909@gmail.com>
2021-07-29 17:13:30 -0400

    Event and Analysis Log Now Have Filter Sections

--- src/components/Logs/AnalysisLog.js
+++ src/components/Logs/AnalysisLog.js
@@ -14,42 +14,47 @@ function AnalysisLog({game_data, frame, frame_list, dimensions}){
 
 
         return (
-            <div  className='AnalysisLog' style = {{height: dimensions.outer_height, width: dimensions.outer_width, 
-                            borderWidth:1, borderStyle:'solid', borderColor:'grey', fontSize:16, overflow:'auto',
-                            marginLeft: 83, direction:'rtl' }} >
-                <h3>Analysis Log</h3>
-                    <ul className='AnalysisLogList'>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                        <li>Hello</li>
-                    </ul>
+            <div className="AnalysisLogAndFilter">
+                 <div className='AnalysisLogFilter' style = {{height: dimensions.outer_height, width: dimensions.outer_width*.2, 
+                    borderWidth:1, borderStyle:'solid', borderColor:'grey', fontSize:16, overflow:'auto'}}>
+                    <h3>Analysis Log - Filter</h3>
+                </div>
+                <div  className='AnalysisLog' style = {{height: dimensions.outer_height, width: dimensions.outer_width*.8, 
+                                borderWidth:1, borderStyle:'solid', borderColor:'grey', fontSize:16, overflow:'auto',direction:'rtl' }} >
+                    <h3>Analysis Log</h3>
+                        <ul className='AnalysisLogList'>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                            <li>Hello</li>
+                        </ul>
+                </div>
             </div>
     
         )
